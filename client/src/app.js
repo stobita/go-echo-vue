656a22c579d087efb305f64ef9659288bbcbe050
stobita <tobita.pc@gmail.com>
2017-10-16 23:23:49 +0900

    コンポーネント化

--- client/src/app.js
+++ client/src/app.js
@@ -1,20 +1,51 @@
 import Vue from 'vue'
 import axios from 'axios'
 
-var app = new Vue({
-  el: '#main-flame',
-  date: {
-    login: false
+let gemail = ''
+
+// プレビュー表示
+Vue.component('preview-email',{
+  template: '<p>email:{{ disemail }}</p>',
+  props: ['disemail']
+})
+
+// 入力フォーム
+Vue.component('email-input',{
+  template: '<input v-on:input="emailInput" v-model="inemail" placeholder="edit me">',
+  data (){
+    return {
+      inemail: ''
+    }
+  },
+  methods: {
+    emailInput: function(){
+      this.$emit('input-email', this.inemail)
+    }
   }
 })
 
-var app = new Vue({
-  el: '#login-form',
-  data: {
-    email: ''
-  },
+// 送信ボタン
+Vue.component('email-submit',{
+  template: '<button v-on:click="createUser" >送信</button>',
   methods: {
     createUser: function() {
+      this.$emit('submit-email')
+    }
+  }
+})
+
+// root インスタンスを作成する
+new Vue({
+  el: '#login-form',
+  data:{
+    email: '',
+    sendResult: ''
+  },
+  methods:{
+    emailValue: function(inEmail){
+      this.email = inEmail
+    },
+    sendEmail: function(){
       axios.post('http://localhost:1323/users/',{
         email: this.email
       })
